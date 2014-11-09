090f670480a8734ce8cfefe5175e1a56f252595e
rangermauve <RangerMauve@hotmail.com>
2014-11-09 15:24:05 -0500

    Fixed up example

--- example.js
+++ example.js
@@ -8,12 +8,12 @@ var streams = new StreamEmitter(events);
 streams.on("message").pipe(process.stdout);
 
 // Relay all `message` events into `message2` using the writable stream from emit
-streams.on("message").pipe(steams.emit("message2"));
+streams.on("message").pipe(streams.emit("message2"));
 
 // Relay all `message2` events to STDOUT as well
 streams.on("message2").pipe(process.stdout);
 
-var times = 15;
+var times = 5;
 var interval = setInterval(function () {
 	// Emit an event on the original event listener
 	events.emit("message", "Ping:" + times + "\n");
